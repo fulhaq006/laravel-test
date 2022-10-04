630342d4085063399f9159bb298f373645c674b4
Farhan Ul Haq <fulhaq006@gmail.com>
2022-10-04 20:27:59 +0500

    inital commit

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+	public function childs() {
+        return $this->hasMany(MenuItem::class,'parent_id','id') ;
+    }
+
+    public function getAll()
+	{
+	    if ($this->childs->count() > 0) {
+	        $this->childs->each->getAll();
+	    }
+	    return $this;
+	}
+}
